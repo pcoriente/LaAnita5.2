1ff429998d8cc711027a9f01f095cb3a66c4b098
carlos.pat <carlos.pat@SistCarlos.laanita.local>
2015-09-09 18:12:50 -0500

    Merge origin/master
    Conflicts:
    	src/java/enumEstatus/EnumEstatus.java

--- src/java/enumEstatus/EnumEstatus.java
+++ src/java/enumEstatus/EnumEstatus.java
@@ -11,11 +11,7 @@ package enumEstatus;
  */
 public enum EnumEstatus {
 
-<<<<<<< HEAD
-    Ninguno(0),
-=======
     Pendiente(0),
->>>>>>> origin/master
     Solicitado(1),
     Rechazado(2),
     Aprobado(3),
