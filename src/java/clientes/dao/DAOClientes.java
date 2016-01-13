fca8869f2aef5a7425beadd1541b30fbfa4160e2
jesc <jesc@T440P.lan>
2016-01-13 09:52:14 -0600

    Correccion al obtener los clientes, marcaba error por datos de la tabla esquema

--- src/java/clientes/dao/DAOClientes.java
+++ src/java/clientes/dao/DAOClientes.java
@@ -55,7 +55,7 @@ public class DAOClientes {
         ArrayList<TOCliente> lstClientes = new ArrayList<>();
         Connection cn = ds.getConnection();
         Statement st = cn.createStatement();
-        String sql = "SELECT C.*, G.grupoCte, G.codigoGrupo, esq.idEsquema, esq.esquema\n"
+        String sql = "SELECT C.*, G.grupoCte, G.codigoGrupo, N.idEsquema, N.esquema\n"
                 + "     , Y.contribuyente, Y.idDireccion AS idDireccionFiscal, R.idRfc, R.rfc, R.curp\n"
                 + "FROM clientes C\n"
                 + "INNER JOIN clientesGrupos G ON G.idGrupoCte=C.idGrupoCte\n"
@@ -80,10 +80,10 @@ public class DAOClientes {
         ArrayList<TOCliente> lstClientes = new ArrayList<>();
         Connection cn = ds.getConnection();
         Statement st = cn.createStatement();
-        String sql = "SELECT C.* , G.grupoCte, G.codigoGrupo, esq.idEsquema, esq.esquema\n"
+        String sql = "SELECT C.* , G.grupoCte, G.codigoGrupo, N.idEsquema, N.esquema\n"
                 + "     , Y.contribuyente, Y.idDireccion AS idDireccionFiscal, R.idRfc, R.rfc, R.curp\n"
                 + "FROM clientes C\n"
-                + "INNER JOIN esquemaNegociacion esq ON esq.idEsquema=C.idEsquema\n"
+                + "INNER JOIN esquemaNegociacion N ON N.idEsquema=C.idEsquema\n"
                 + "INNER JOIN clientesGrupos G ON G.idGrupoCte=C.idGrupoCte \n"
                 + "INNER JOIN contribuyentes Y ON Y.idContribuyente = C.idContribuyente\n"
                 + "INNER JOIN contribuyentesRfc R ON R.idRfc = Y.idRfc ORDER BY C.idGrupoCte, Y.contribuyente";
